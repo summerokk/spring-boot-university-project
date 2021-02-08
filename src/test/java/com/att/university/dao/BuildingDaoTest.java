44328d629a8627397156467a618cf802c8187b28
S U M M E R <summerokk@gmail.com>
2021-02-08 14:37:23 +0300

    config, tests, controller

--- src/test/java/com/att/university/dao/BuildingDaoTest.java
+++ src/test/java/com/att/university/dao/BuildingDaoTest.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.datasource.init.DatabasePopulatorUtils;
 import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
-import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
