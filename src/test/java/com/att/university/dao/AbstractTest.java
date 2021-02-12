35d667bc507b6764a053c93da3e439ff98dd87b7
S U M M E R <summerokk@gmail.com>
2021-02-13 01:47:45 +0300

    static file names

--- src/test/java/com/att/university/dao/AbstractTest.java
+++ src/test/java/com/att/university/dao/AbstractTest.java
@@ -7,9 +7,12 @@ import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 import javax.sql.DataSource;
 
 public abstract class AbstractTest {
+    private static final String TABLE_FILE_NAME = "Table.sql";
+    private static final String TEST_DATA_FILE_NAME = "TestData.sql";
+
     protected void recreateDb(DataSource dataSource) {
         ResourceDatabasePopulator databasePopulator = new ResourceDatabasePopulator();
-        databasePopulator.addScripts(new ClassPathResource("Table.sql"), new ClassPathResource("TestData.sql"));
+        databasePopulator.addScripts(new ClassPathResource(TABLE_FILE_NAME), new ClassPathResource(TEST_DATA_FILE_NAME));
         DatabasePopulatorUtils.execute(databasePopulator, dataSource);
     }
 }
