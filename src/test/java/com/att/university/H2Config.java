6f552bbb05b29be3a9b9ab524857027915e46bf8
S U M M E R <summerokk@gmail.com>
2021-02-13 15:37:06 +0300

    h2config

--- src/test/java/com/att/university/H2Config.java
+++ src/test/java/com/att/university/H2Config.java
@@ -1,10 +1,10 @@
 package com.att.university;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
@@ -17,18 +17,18 @@ import javax.sql.DataSource;
 @PropertySource("classpath:h2.properties")
 public class H2Config {
     @Bean
-    public JdbcTemplate jdbcTemplate(DataSource source) {
+    public JdbcTemplate jdbcTemplate(@Qualifier("dataSourceTest") DataSource source) {
         return new JdbcTemplate(source);
     }
 
     @Bean
-    @Primary
     public DataSource dataSourceTest(@Value("${h2.name}") String name) {
         return new EmbeddedDatabaseBuilder()
                 .setName(name)
                 .setType(EmbeddedDatabaseType.H2)
                 .setScriptEncoding("UTF-8")
                 .ignoreFailedDrops(true)
+                .addScripts("tables.sql", "TestData.sql")
                 .build();
     }
 }
